3a2f9a69dbe523ee65da9de3dd56d33d697dda3d
ranyery <ranyerysc@gmail.com>
2022-08-22 19:07:02 -0300

    feat: adiciona listagem de categorias

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -19,6 +19,10 @@ class CategoriesRepository {
 
     this.categories.push(category);
   }
+
+  public list(): Category[] {
+    return this.categories;
+  }
 }
 
 export { CategoriesRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -13,4 +13,10 @@ categoriesRoutes.post("/", (request, response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get("/", (request, response) => {
+  const categories = categoriesRepository.list();
+
+  response.status(200).json(categories);
+});
+
 export { categoriesRoutes };
