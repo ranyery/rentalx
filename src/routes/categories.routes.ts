167031e72d11b78f39d07cadd056b696dae85063
ranyery <ranyerysc@gmail.com>
2022-08-22 19:53:26 -0300

    feat: adiciona servico responsavel por criar categorias e organiza responsabilidades

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,13 +9,15 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category already exists!" });
+  try {
+    const createCategoryService = new CreateCategoryService(
+      categoriesRepository
+    );
+    createCategoryService.execute({ name, description });
+  } catch (error: any) {
+    const message = error?.message;
+    return response.status(400).send({ message });
   }
-
-  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
