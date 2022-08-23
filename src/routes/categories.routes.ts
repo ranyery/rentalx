1651e6d8a0d42bcba66030474718d056e6f9cf44
ranyery <ranyerysc@gmail.com>
2022-08-22 21:43:21 -0300

    refactor: ajusta caso de uso de criacao de categorias e organiza responsabilidades

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,12 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  try {
-    const createCategoryService = new CreateCategoryService(
-      categoriesRepository
-    );
-    createCategoryService.execute({ name, description });
-  } catch (error: any) {
-    const message = error?.message;
-    return response.status(400).send({ message });
-  }
-
-  return response.status(201).send();
-});
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", (request, response) => {
   const categories = categoriesRepository.list();
