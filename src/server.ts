f05cd8038e14507c832eb7405ca55c4f494d2dda
ranyery <ranyerysc@gmail.com>
2022-08-23 08:42:12 -0300

    refactor: organiza conjunto de rotas da aplicacao

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const PORT = process.env.PORT || 3333;
-
 const app = express();
 
 app.use(express.json());
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
